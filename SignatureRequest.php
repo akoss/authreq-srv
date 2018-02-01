e2b38015e1df2312f09d01bf1d2f3ff2ff05af64
Akos Szente <akos@szente.info>
2018-02-01 16:51:19 +0100

    Adding sha256 hash to requests. Heavy refactoration.

---
+++ SignatureRequest.php
@@ -0,0 +1,93 @@
+<?php 
+class SignatureRequest { 
+	public $nonce;
+	public $push_title;
+	public $push_subtitle;
+	public $push_category;
+	public $push_text;
+	public $short_title;
+	public $message_id;
+	public $reply_status;
+	public $response_url;
+	public $timestamp;
+	public $reply_timestamp;
+	private $expiry;
+
+	private $srv_signature;
+	public $expiry_in_seconds;
+
+	function getBencode() {
+		return BEncode::encode([
+			'body'=>$this->push_text,
+			'title'=>$this->push_title,
+			'subtitle'=>$this->push_subtitle,
+			'category'=>$this->push_category,
+			'response_url'=>$this->response_url,
+			'message_id'=>$this->message_id,
+			'short_title'=>$this->short_title,
+			'nonce'=>$this->nonce,
+			'expiry'=>$this->expiry,
+		]);
+	}
+
+	function getSignature() {
+		$bencoded = $this->getBencode();
+		$hash = base64_encode(hash("sha256", $bencoded, true));
+
+		return $hash;
+	}
+
+	function getBencodeForSignature() {
+
+	}
+
+	function setupWith($service_provider_name, $message_id, $response_url, $long_description, $short_description, $nonce = null, $expiry = null){
+		$this->push_text = $long_description; 
+		$this->push_subtitle = $service_provider_name;
+		$this->expiry_in_seconds = $expiry; 
+		$this->short_title = $short_description;
+		$this->message_id = $message_id; 
+		$this->nonce = $nonce; 
+		$this->response_url = $response_url; 
+		$this->expiry = time() + $this->expiry_in_seconds;
+		$this->push_category = 'challengecategory';
+
+		$this->push_title = json_decode('"\uD83D\uDD35"') . " " . 'New Signature Request';
+
+		$this->srv_signature = $this->getSignature(); 
+	}
+
+	function sendPush($token) {
+		$push = new ApnsPHP_Push(
+			ApnsPHP_Abstract::ENVIRONMENT_SANDBOX,
+			'both.pem'
+		);
+		$push->setLogger(new ApnsPHP_Log_Silent());
+		$push->setRootCertificationAuthority('entrust_root_certification_authority.pem');
+		$push->connect();
+		$message = new ApnsPHP_Message($token); // X
+		//$message = new ApnsPHP_Message('fc27299d7a17ef04da8fb4448bca82f4ca620ea0134537c3cdce0f017c6e5a05'); // 5s
+
+		$message->setBadge(1);
+
+		$message->setText($this->push_text);
+		$message->setTitle($this->push_title);
+		$message->setSubtitle($this->push_subtitle);
+		$message->setCategory($this->push_category);
+		$message->setContentAvailable(true);
+		$message->setSound();
+		//throw new Exception($this->srv_signature);
+		$message->setCustomProperty('additional_data', array('bencode' => $this->getBencode(), 'expiry' => $this->expiry, 'short_title' => $this->short_title, 'signature' => $this->srv_signature, 'message_id' => $this->message_id, 'nonce' => $this->nonce, 'response_url'=> $this->response_url));
+
+		$message->setExpiry($this->expiry_in_seconds);
+		$push->add($message);
+		$push->send();
+		$push->disconnect();
+
+		$aErrorQueue = $push->getErrors();
+		if (!empty($aErrorQueue)) {
+			var_dump($aErrorQueue);
+		}
+	}
+}
+?>
--- index.php
+++ index.php
@@ -1,36 +1,28 @@
 <?php
 
-date_default_timezone_set('Europe/Budapest');
-
+include('SignatureRequest.php');
+require 'bencode.php';
 require_once 'ApnsPHP/Autoload.php';
 
-$push = new ApnsPHP_Push(
-	ApnsPHP_Abstract::ENVIRONMENT_SANDBOX,
-	'both.pem'
+date_default_timezone_set('Europe/Budapest');
+
+$signatureRequest = new SignatureRequest;
+
+$signatureRequest->setupWith(
+	$service_provider_name = 'Nationwide', 
+	$message_id = 2341234, 
+	$response_url = 'http://192.168.100.139:8080/authreq-srv/callback.php', 
+	$long_description = 'Sending £46.00 to Mr L Balog from your FlexDirect debit account on 13rd Oct, 16:07', 
+	$short_description = '£46.00 to Mr L Balog', 
+	$nonce = 'pinafül7', 
+	$expiry = 60
 );
-$push->setLogger(new ApnsPHP_Log_Silent());
-$push->setRootCertificationAuthority('entrust_root_certification_authority.pem');
-$push->connect();
-$message = new ApnsPHP_Message('5b54f9bd4215c3ba405a200d7e4353946f60e7878db7334b4cdc94bf53dbce52'); // X
-//$message = new ApnsPHP_Message('fc27299d7a17ef04da8fb4448bca82f4ca620ea0134537c3cdce0f017c6e5a05'); // 5s
 
-//$message->setBadge(1);
+echo $signatureRequest->getBencode();
 
-$message->setText('Sending £46.00 to Mr L Balog from your FlexDirect debit account on 13rd Oct, 16:07');
-$message->setTitle(json_decode('"\uD83D\uDD35"') . " New Signature Request");
-$message->setSubtitle('Nationwide');
-$message->setCategory('challengecategory');
-$message->setContentAvailable(true);
-$message->setSound();
-$message->setCustomProperty('additional_data', array('expiry' => time() + 15, 'short_title' => '£45.00 to Mr ' . time(), 'signature' => 'to_be_filled', 'message_id' => 2341234, 'nonce' => 'pinafül7', 'response_url'=> 'http://192.168.100.139:8080/authreq-srv/callback.php'));
-$message->setExpiry(60);
-$push->add($message);
-$push->send();
-$push->disconnect();
+echo $signatureRequest->getSignature();
 
-$aErrorQueue = $push->getErrors();
-if (!empty($aErrorQueue)) {
-	var_dump($aErrorQueue);
-}
+// $signatureRequest->sendPush($token = '5b54f9bd4215c3ba405a200d7e4353946f60e7878db7334b4cdc94bf53dbce52');
+$signatureRequest->sendPush($token = '53202ca1d0276be7084b7e4dc760bba70500a4ec5937149cee9a1fa889f5cec0');
 
 echo "<h1>Fin</h1>";
